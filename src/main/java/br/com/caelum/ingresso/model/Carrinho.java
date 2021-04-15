48a836f3e09c31f5507617da02844c4b56d9a637
eb.tayson <tayson.assis@eb.mil.br>
2021-04-15 10:30:49 -0300

    exibição da tela de compra

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +12,13 @@ import org.springframework.web.context.annotation.SessionScope;
 public class Carrinho {
 
 	private List<Ingresso> ingressos = new ArrayList<>();
-	
+
 	public void add(Ingresso ingresso) {
 		ingressos.add(ingresso);
+	}
+
+	public BigDecimal getTotal() {
+		return ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
 	}
 
 	public List<Ingresso> getIngressos() {
